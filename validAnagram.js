d43cd0e228c03b09084ae149424fd6ab9c63a732
aniketpati <aniketp209@gmail.com>
2023-03-21 01:49:16 +0530

    Added Code for sum zero

--- validAnagram.js
+++ validAnagram.js
@@ -1,22 +1,15 @@
 function validAnagram(str1, str2)
 {
-    if (str1.length === 0 && str2.length === 0) return true;
     if (str1.length !== str2.length) return false;
-
-    let frequencyCounter1 = {};
-    let frequencyCounter2 = {};
+    const lookup = {};
     for (let char of str1)
     {
-        frequencyCounter1[char] = (frequencyCounter1[char] || 0) + 1;
+        lookup[char] ? lookup[char] += 1 : lookup[char] = 1;
     }
     for (let char of str2)
     {
-        frequencyCounter2[char] = (frequencyCounter2[char] || 0) + 1;
-    }
-    for (let key in frequencyCounter1)
-    {
-        if (!(key in frequencyCounter2)) return false;
-        if (frequencyCounter1[key] !== frequencyCounter2[key]) return false;
+        if (!lookup[char]) return false;
+        else lookup[char] -= 1;
     }
     return true;
 }
